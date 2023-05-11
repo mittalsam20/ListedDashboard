db1bc1cab3fb1222cdba326ba96b7102d10a44d2
samaksh mittal <sam.mittal44@gmail.com>
2023-05-12 02:25:56 +0530

    feat: add header partial for applayout

--- client/src/Layouts/Partials/AppPartials/Header/Header.js
+++ client/src/Layouts/Partials/AppPartials/Header/Header.js
@@ -1,9 +1,21 @@
 import React from "react";
 
 import classes from "./Header.module.scss";
+import Avatar from "UIComponents/Avatar";
+import BellIcon from "SvgComponents/BellIcon";
+import SearchBar from "AppComponents/SearchBar";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className={classes.headerContainer}>
+      <div className={classes.pageTitle}>{"Dashboard"}</div>
+      <div className={classes.rightContainer}>
+        <SearchBar />
+        <BellIcon />
+        <Avatar />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
