d7d91a5a1cc0612c21c02b2b82002e853fceb2c5
samaksh mittal <sam.mittal44@gmail.com>
2023-05-11 19:36:52 +0530

    feat: add UIButton component

---
+++ client/src/UIComponents/UIButton/UIButton.js
@@ -0,0 +1,131 @@
+import React from "react";
+
+import PropTypes from "prop-types";
+import classNames from "classnames";
+import { fontColors, backgroundColors } from "Constants";
+
+import classes from "./UIButton.module.scss";
+
+const { bgLightTeal, bgDefault, bgLightRed, bgBlack } = backgroundColors;
+
+const variantToStylesMapping = {
+  success: bgLightTeal,
+  primary: bgDefault,
+  danger: bgLightRed,
+  dark: { backgroundColor: bgBlack, color: fontColors.textWhite },
+  plain: "transparent",
+};
+
+const sizeToHeightMapping = {
+  xs: "24px",
+  sm: "28px",
+  md: "32px",
+  lg: "40px",
+  xl: "48px",
+};
+
+const getButtonContainerStyle = ({
+  size,
+  variant,
+  isDisabled,
+  containerStyle,
+}) => {
+  let buttonContainerStyle = {
+    height: sizeToHeightMapping[size] || sizeToHeightMapping.md,
+    ...(variantToStylesMapping[variant] || sizeToHeightMapping.primary),
+  };
+
+  if (isDisabled)
+    buttonContainerStyle = {
+      ...buttonContainerStyle,
+      backgroundColor: backgroundColors.bgSubtle,
+      cursor: "not-allowed",
+    };
+  return { ...buttonContainerStyle, ...containerStyle };
+};
+
+const getChildComponent = ({ Icon, iconPosition, children }) => {
+  // Since the two screens are only using FrontIconPosition so I am not writing a switch statement for rest all cases
+  return (
+    <>
+      {Icon}
+      {children}
+    </>
+  );
+};
+
+const UIButton = React.memo(props => {
+  const {
+    size,
+    variant,
+    children,
+    onClick,
+    Icon = null,
+    iconPosition,
+    className,
+    isDisabled,
+    isIconButton,
+    containerStyle,
+  } = props;
+
+  const buttonContainerClass = classNames(
+    { [className]: !!className },
+    { [classes.buttonContainer]: true },
+    { [classes.iconButtonContainer]: isIconButton }
+  );
+
+  const childComponent = Icon
+    ? getChildComponent({ Icon, iconPosition, children })
+    : children;
+
+  const buttonContainerStyle = getButtonContainerStyle({
+    size,
+    variant,
+    isDisabled,
+    containerStyle,
+  });
+
+  const handleOnClick = event => {
+    if (isDisabled) {
+      event.stopPropagation();
+      return;
+    }
+    onClick(event);
+  };
+
+  return (
+    <div
+      onClick={handleOnClick}
+      style={buttonContainerStyle}
+      className={buttonContainerClass}
+    >
+      {childComponent}
+    </div>
+  );
+});
+
+const buttonSizes = ["xl", "lg", "md", "sm", "xs"];
+const iconPositions = ["left", "right", "top", "bottom"];
+const buttonVariants = ["success", "primary", "warning", "danger", "plain"];
+
+UIButton.propTypes = {
+  size: PropTypes.oneOf(buttonSizes),
+  variant: PropTypes.oneOf(buttonVariants),
+  onClick: PropTypes.func,
+  iconPosition: PropTypes.oneOf(iconPositions),
+  isDisabled: PropTypes.bool,
+  containerStyle: PropTypes.object,
+  isIconButton: PropTypes.bool,
+};
+
+UIButton.defaultProps = {
+  size: "md",
+  variant: "primary",
+  onClick: () => {},
+  iconPosition: "left",
+  isDisabled: false,
+  containerStyle: {},
+  isIconButton: false,
+};
+
+export default UIButton;
